45be8769bb3262d7d9af0666f8dddb3e5ca8b4a9
shksantosh <santoshking82@gmail.com>
2024-01-20 01:28:11 +0530

    Protect Routes & Authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -70,6 +70,18 @@ const loginUser = asyncHandler(async (req, res) => {
     }
 })
 
+// @Description  Get current user
+// @Route /api/users/me
+// @acess Private
+const getMe = asyncHandler(async (req, res) => {
+    const user = {
+        id: req.user._id,
+        email: req.user.email,
+        name: req.user.name
+    }
+    res.status(200).json(user)
+})
+
 // Generate token 
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -79,5 +91,6 @@ const generateToken = (id) => {
 
 module.exports = {
     registerUser,
-    loginUser
+    loginUser, 
+    getMe
 }
