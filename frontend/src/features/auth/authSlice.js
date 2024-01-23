5541e3e840a16b9f6663da388d802be20428e973
shksantosh <santoshking82@gmail.com>
2024-01-23 20:55:38 +0530

    Hook Register Form To Redux

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -5,8 +5,20 @@ const initialState = {
     isError: false,
     isSuccess: false,
     isLoading: false,
-    message: ''
+    message: '',
 }
+
+// Register new user
+export const register = createAsyncThunk(
+    'auth/register',
+    async (user, thunkAPI) => {
+        console.log(user);
+})
+
+// Login user
+export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
+    console.log(user)
+  })
 
 export const authSlice = createSlice({
     name: 'auth',
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
-import { toast } from "react-toastify"
 import { FaSignInAlt } from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux'
+import { login } from '../features/auth/authSlice'
+
 function Login() {
     const [formData, setFromData] = useState({
         email: '',
@@ -8,6 +10,12 @@ function Login() {
     })
 
     const {email, password} = formData
+
+    const dispatch = useDispatch()
+
+  const { user, isLoading, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
 
     const onChange = (e) => {
         setFromData((prevState) => ({
@@ -18,6 +26,13 @@ function Login() {
 
     const onSubmit = (e) => {
         e.preventDefault()
+
+        const userData = {
+          email,
+          password,
+        }
+    
+        dispatch(login(userData))
       } 
 
     return (
